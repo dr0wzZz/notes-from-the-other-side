ba3710ce6e9f3ae879cb98e4589ff99e600881b4
Ramon <roaduru@gmail.com>
2025-05-28 12:44:08 -0700

    entriee

--- entries.js
+++ entries.js
@@ -2,15 +2,15 @@ const journalEntries = [
   {
     id: 1,
     title: "Journal entries and thoughts",
-    content: "Looks like my life has finally crumbled so hard that I'm living on the streets now. I decided to make a blog I guess to try and scream into the void. So far it's been shit, but I'm hoping it'll only get worse. see you next time",
-    timestamp: "2025-05-27T22:30:00",
+    content: "Looks like my life has finally crumbled so hard that I'm living on the streets now. I decided to make a blog I guess to try and scream into the void. So far it's been shit, but I'm hoping it'll only get worse. i still have my cell phone so i can do shit on the internet still if i find wifi. made this website with chatgpt. see you next time",
+    timestamp: "2025-05-27T22:30",
     image: "assets/images/huuse.jpg"
   },
   {
     id: 2,
-    title: "Another Day, Another Decibel",
-    content: "Woke up to Slayer. Breakfast was leftover nachos and distortion pedals.",
-    timestamp: "2025-05-28T09:15:00",
+    title: "Another day, another dollar",
+    content: "Woke up to Cops kicking me out of the park again. don't know why they came so early. i think someone is following me. walking to downtown to make some money, i guess its good i'm awake so early",
+    timestamp: "2025-05-28T06:15",
     image: ""
   }
 ];
